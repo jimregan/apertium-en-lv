369bd98f8edaf8996359cd29347520160403733e
Jim O'Regan <joregan@gmail.com>
2010-09-07 23:55:31 +0000

    delete obviously incorrect entries

--- dev/fixer.pl
+++ dev/fixer.pl
@@ -6,14 +6,9 @@ use strict;
 my $parname;
  
 while (<>) {
-	print;
-	if (/<pardef n="([^"]*)"/) {$parname = $1};
-	next if (/"ref"/);
-	if (s!<s n="pri"/><s n="p1"/><s n="pl"/></r></p>!</r></p><par n="zogam/s__pprs_psv"/>!) {
-		if (/<l>([^<]*)<\/l>[^>]*<r>([^<]*)</ && ($1 eq $2)) {
-			print STDERR "Paradigm error: $parname\n";
-		} else {
-			print;
-		}
+	if (/<l>([^<]*)<\/l>[^>]*<r>([^<]*)</ && ($1 eq $2)) {
+		print if (/"inf"/);
+	} else {
+		print;
 	}
 }
